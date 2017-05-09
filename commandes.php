11205f9d3e931010a47ef051f5f3d97daec042fb
KWEEZWD <ruben.aramos@hotmail.com>
2017-05-09 11:40:26 +0200

    Modification contenue

--- commandes.php
+++ commandes.php
@@ -42,8 +42,8 @@
         </nav>
         <div class="contenu">
             <div class="commande">
-                <h2 class="titreCommande">Américain</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <h2 class="titreCommande">Cheeseburger</h2><hr>
+                <p>Ingrédients : Pain, Steak Hachée, Fromage Cheddar, Oignons, Salade, Tomates, Sauce Ketchup et Moutarde</p>
             </div>
             <div class="commande">
                 <h2 class="titreCommande">Américain</h2>
