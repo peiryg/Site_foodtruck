08937ffaa561b65ce395b1df81d79447cf1acf96
peiryg <tehxso7@gmail.com>
2017-05-23 09:05:31 +0200

    modal

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require 'modal.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,12 +9,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Foodriders</title>      
         <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+
         <link href="style.css" rel="stylesheet" type="text/css"/>
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
         <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -31,7 +34,29 @@
                         </h2>
                         <div class="row-menu-wrap">
                             <div class="row-menu">
-                                <a href="#" class="item-menu">Cheese</a>
+                                <p class="item-menu">Cheese</p>
+                                <span href="#" class="item-prise">10.-</span>
+                                <button class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Ajouter</button>
+                            </div>
+                            <span class="red-line"></span>
+                        </div>
+                        <div class="row-menu-wrap">
+                            <div class="row-menu">
+                                <p class="item-menu">Cheese</p>
+                                <span href="#" class="item-prise">10.-</span>
+                                <button class="btn">Ajouter</button>
+                            </div>
+                            <span class="red-line"></span>
+                        </div><div class="row-menu-wrap">
+                            <div class="row-menu">
+                                <p class="item-menu">Cheese</p>
+                                <span href="#" class="item-prise">10.-</span>
+                                <button class="btn">Ajouter</button>
+                            </div>
+                            <span class="red-line"></span>
+                        </div><div class="row-menu-wrap">
+                            <div class="row-menu">
+                                <p class="item-menu">Cheese</p>
                                 <span href="#" class="item-prise">10.-</span>
                                 <button class="btn">Ajouter</button>
                             </div>
@@ -39,18 +64,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-5 left">
-                    <div class="wrap-menu">
-                        <div class="row-menu-wrap">
-                            <div class="row-menu">
-                                <a href="#" class="item-menu">Tzatsiki</a>
-                                <span href="#" class="item-prise"> &nbsp;&nbsp;10.-</span>
-                            </div>
-                            <span class="red-line"></span>
-                            <span class="item-descr">Refreshing traditional cucumber and garlic yoghurt dip.</span>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
         <footer>
@@ -58,7 +72,6 @@
                 <img class="img_footer" src="img/logo.png" alt=""/>
                 test
             </div>
-
         </footer>
     </body>
 </html>
---
+++ modal.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Index</title>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h1  class="titre" style="font-size: 40pt;"> LOS TARDOS</h1>
+                    </div>
+                    <div class="modal-body">
+                        <a href='https://github.com/peiryg/TutoGIT'>Le projet github</a><br><br>
+
+                        <p>
+                            Site crée par : <ul>
+                            <li>Daaqi</li>
+                            <li>KweezWD</li>
+                            <li>Bad Motherfucker</li>
+
+                        </ul>
+                        
+                        
+                        
+                        <p style="font-weight: bold;text-align: right;">
+                            <span class="glyphicon glyphicon-envelope"></span><a href="mailto:supermarl9wp@gmail.com">supermarl9wp@gmail.com</a>
+                        </p>
+
+                    </div>
+                    <div class="modal-footer" style="text-align: center;">
+
+                        &copy Los Tardos Production
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
